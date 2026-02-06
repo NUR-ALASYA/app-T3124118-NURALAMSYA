4f1518617c4d05f2a6d05a93e566627ebb5e1e0a
NUR-ALAMSYA <nuralamsya909@gmail.com>
2026-02-06 15:25:06 +0800

    Materi Dua

--- materi-kedua/demonstrasi-code.php
+++ materi-kedua/demonstrasi-code.php
@@ -3,18 +3,15 @@ class Mahasiswa {
     public $nim;
     public $nama;
     public $prodi;
-
     public function __construct($nim, $nama, $prodi) {
         $this->nim = $nim;
         $this->nama = $nama;
         $this->prodi = $prodi;
     }
-
     public function getProfile() {
         return "[$this->nim] $this->nama - $this->prodi";
     }
 }
-
 $m1 = new Mahasiswa("TI24118", "ALAM", "Teknik Informatika");
 echo $m1->getProfile();
 ?>
